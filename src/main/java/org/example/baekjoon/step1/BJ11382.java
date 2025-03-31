46851a63394f93388b2eae7b727ee98f691cfc1a
homea90 <homea90@naver.com>
2025-03-31 13:23:44 +0900

    백준 - 입출력과 사칙연산
    11번 -11382문제

---
+++ src/main/java/org/example/baekjoon/step1/BJ11382.java
@@ -0,0 +1,20 @@
+package org.example.baekjoon.step1;
+
+import java.util.Scanner;
+
+public class BJ11382 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        String[] inputNum = input.split(" ");
+
+        long a = Long.parseLong(inputNum[0]);
+        long b = Long.parseLong(inputNum[1]);
+        long c = Long.parseLong(inputNum[2]);
+
+
+        System.out.println(a + b + c);
+    }
+}
