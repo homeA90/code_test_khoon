3431e1bb0af30728dd0ce946f8b75f9f4cfab3b1
homea90 <homea90@naver.com>
2025-03-31 10:46:00 +0900

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> list = new ArrayList<>();
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+
+        while (b > 0) {
+            list.add(b % 10);
+            b = b / 10;
+        }
+
+        int answer1 = a * list.get(0);
+        int answer2 = a * list.get(1);
+        int answer3 = a * list.get(2);
+        int answer4 = answer1 + (answer2 * 10) + (answer3 * 100);
+
+        System.out.println(answer1);
+        System.out.println(answer2);
+        System.out.println(answer3);
+        System.out.println(answer4);
+
+    }
+}
