f5da05833a382c3f8c46553b4c41b2a509a16e0c
homea90 <homea90@naver.com>
2025-03-31 14:40:10 +0900

    백준 - 입출력과 사칙연산
    12번 -10171문제

---
+++ src/main/java/org/example/baekjoon/step1/BJ10171.java
@@ -0,0 +1,15 @@
+package org.example.baekjoon.step1;
+
+public class BJ10171 {
+    public static void main(String[] args) {
+
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
+//        \    /\
+//         )  ( ')
+//        (  /  )
+//         \(__)|
